9ba12c85752b193578118b98d2816b342a419443
BlackStarCodes <301748195+BlackStarCodes@users.noreply.github.com>
2026-07-06 20:28:35 +0530

    earlier when token-sub was username. so when a user updates their username, their current token becomes useless/wrong cause token sub contains old username but db contains updated username. so changed token-sub to user_id, functions uses user_id in the token to check. no-more broken token

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -18,5 +18,5 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password!", headers={"WWW-Authenticate": "Bearer"})
     access_token_expires = timedelta(minutes=TOKEN_EXPIRE_MINS)
-    access_token = create_access_token(data={"sub":user.name}, expires_delta= access_token_expires)
+    access_token = create_access_token(data={"sub":str(user.id)}, expires_delta= access_token_expires)
     return Token(access_token=access_token, token_type="bearer")
--- src/security.py
+++ src/security.py
@@ -49,13 +49,13 @@ def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], session: ses
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials!", headers={"WWW-Authenticate": "Bearer"})
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
-        username = payload.get("sub")
-        if username is None:
+        user_id = int(payload.get("sub"))
+        if user_id is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        token_data = TokenData(id=user_id)
     except InvalidTokenError:
         raise credentials_exception
-    user = session.scalar((select(UserORM).where(UserORM.name == token_data.username)))
+    user = session.get(UserORM, token_data.id)
     if user is None:
         raise credentials_exception
     return user
